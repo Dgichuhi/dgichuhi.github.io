7505b16170ac6239f0d2ea9fc42b96169ba36a5d
Dominic Gichuhi <gichuhimd@gmail.com>
2017-01-10 10:30:42 +0300

    Update Detail.controller.js

--- view/Detail.controller.js
+++ view/Detail.controller.js
@@ -1,12 +1,18 @@
-sap.ui.controller("sap.ui.demo.myFiori.view.Detail", {
-
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/ui/core/routing/History",
+	'sap/m/Dialog', 'sap/m/Button','sap/m/Text'
+], function (Controller, MessageToast, History, Dialog, Button, Text) {
+	"use strict";
+ 
+	return Controller.extend("sap.ui.demo.myFiori.view.Detail", {
 		onInit: function() {
 		
 		this.router = sap.ui.core.UIComponent.getRouterFor(this);
 		this.router.attachRoutePatternMatched(this._handleRouteMatched, this);
 		
 	},
-
 _handleRouteMatched: function(evt) {
 			
 		this.detIndex = evt.getParameter("arguments").detIndex;
@@ -23,10 +29,7 @@ _handleRouteMatched: function(evt) {
 	h123andleNavButtonPress : function (evt) {
 		this.nav.back("Master");
 	},
-	/*,
-	onBeforeRendering: function(){
-this.byId("SupplierForm").bindElement("/MypracticeNavigation");
-}*/
+	
 
 
 			handleNavButtonPress : function () {
@@ -42,5 +45,26 @@ this.byId("SupplierForm").bindElement("/MypracticeNavigation");
 				oRouter.navTo("master", true);
 			}
 		
-	}
+	},
+	onMessageDialogPress: function (oEvent) {
+			 var dialog = new Dialog({
+                title: 'A business application from us?',
+                type: 'Message',
+                    content: new Text({
+                        text: 'Call/Whatsapp +254 702 495 991'
+                    }),
+                beginButton: new Button({
+                    text: 'OK',
+                    press: function () {
+                        dialog.close();
+                    }
+                }),
+                afterClose: function() {
+                    dialog.destroy();
+                }
+            });
+ 
+			dialog.open();
+		}
 });
+	});
